aee7ce4e6b369e9234f54d436e10288a40d7d4cd
Bollwerker <Bollwerker@users.noreply.github.com>
2014-11-30 17:12:11 +0100

    Update ClientServerConDB.java

--- ClientServerConDB.java
+++ ClientServerConDB.java
@@ -9,6 +9,7 @@ import java.io.*;
  */
 public class ClientServerConDB extends RequestServer
 {
+    //Aufsetzen der notwendigen Variablen und Objekte für Socketverbindungen
     private static Socket socket;
     //private static FileOutputStream fileOut;
     //private static FileInputStream fileIn;
@@ -18,21 +19,24 @@ public class ClientServerConDB extends RequestServer
     
     public static ArrayList minorCon (long time, boolean truefalse)
     {
+        //Einsetzen der Übergabe Variablen in ein Objekt, zum vereinfachen der Verbindung
         LongBool longBool = new LongBool(time,truefalse);
         try
         {
+            //start der Übertragung
             socket = new Socket("localhost",1);
             //fileOut = new FileOutputStream("/tmp/longBool.ser");
             oOut = new ObjectOutputStream(socket.getOutputStream());
             oOut.writeObject(longBool);
-            oOut.flush();
+            oOut.flush();   //flush um verbleibende Daten zu vollen Paketen aufzufüllen und heraus zu schieben
             oOut.close();
-            
+            //Übergang von Senden zu Empfangen
             oIn =  new ObjectInputStream(socket.getInputStream());
             try{answer = (ArrayList) oIn.readObject();}catch(ClassNotFoundException e){System.out.println(e);}
             oIn.close();
 
             socket.close();
+            //ende der Übertragung
         }
         catch(IOException e)
         {
@@ -44,18 +48,20 @@ public class ClientServerConDB extends RequestServer
     {
         try
         {
+            //start der Übertragung
             socket = new Socket("localhost",1);
             //fileOut = new FileOutputStream("/tmp/longBool.ser");
             oOut = new ObjectOutputStream(socket.getOutputStream());
             oOut.writeObject(truefalse);
             oOut.flush();
             oOut.close();
-            
+            //Übergang von Senden zu Empfangen
             oIn =  new ObjectInputStream(socket.getInputStream());
             try{answer = (ArrayList) oIn.readObject();}catch(ClassNotFoundException e){System.out.println(e);}
             oIn.close();
 
             socket.close();
+            //ende der Übertragung
         }
         catch(IOException e)
         {
@@ -65,21 +71,24 @@ public class ClientServerConDB extends RequestServer
     }
     public static ArrayList numberCon (int number, boolean truefalse)
     {
+        //Einsetzen der Übergabe Variablen in ein Objekt, zum vereinfachen der Verbindung
         IntBool intBool = new IntBool(number,truefalse);
         try
         {
+            //start der Übertragung
             socket = new Socket("localhost",1);
             //fileOut = new FileOutputStream("/tmp/longBool.ser");
             oOut = new ObjectOutputStream(socket.getOutputStream());
             oOut.writeObject(intBool);
             oOut.flush();
             oOut.close();
-            
+            //Übergang von Senden zu Empfangen
             oIn =  new ObjectInputStream(socket.getInputStream());
             try{answer = (ArrayList) oIn.readObject();}catch(ClassNotFoundException e){System.out.println(e);}
             oIn.close();
 
             socket.close();
+            //ende der Übertragung
         }
         catch(IOException e)
         {
