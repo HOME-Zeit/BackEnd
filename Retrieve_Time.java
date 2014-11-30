4c291c26181f47c742abcddee07ff0d2e0358862
Bollwerker <Bollwerker@users.noreply.github.com>
2014-11-30 18:11:23 +0100

    Update Retrieve_Time.java

--- Retrieve_Time.java
+++ Retrieve_Time.java
@@ -10,20 +10,25 @@ import org.apache.commons.net.ntp.TimeInfo;
  */
 public class Retrieve_Time 
 {
+    //festlegen eines Zeitservers, jederzeit hier änderbar, nicht allgemeiner standard
     final private static String TIME_SERVER = "time-a.nist.gov";
 
     public static long getTime() throws Exception 
     {
+        //verbindung zum NTP aufbauen 
         NTPUDPClient timeClient = new NTPUDPClient();
         InetAddress inetAddress = InetAddress.getByName(TIME_SERVER);
         TimeInfo timeInfo = timeClient.getTime(inetAddress);    //org.apache.commons.net.ntp.TimeInfo@2503dbd3
+        //erlangte Daten auf epoch zuschneiden
         long returnTime = timeInfo.getReturnTime();             //1415862544656
         //Date time = new Date(returnTime);                     //Thu Nov 13 08:09:04 CET 2014
         
         return returnTime;
     }
+    //bei nichtverfügbarkeit des NTP die lokale zeit benutzen
     public static long getTime_failed()
     {
+        //aktuelle systemzeit beziehen
         long time = System.currentTimeMillis();
         return time;
     }
