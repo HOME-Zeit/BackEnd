858263d88b1ff91da5eb01cb47af6cf26bc85c94
Bollwerker <Bollwerker@users.noreply.github.com>
2014-11-30 18:05:50 +0100

    Update LongBool.java

--- LongBool.java
+++ LongBool.java
@@ -15,3 +15,4 @@ public class LongBool implements Serializable
         this.truefalse = truefalse;
     }
 }
+//Anlegen von Constructor um Übergabevariablen in ein einzelnes Objekt zu binden um die Übertragung zu erleichtern
