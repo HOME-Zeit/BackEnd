1136bf5984c88ff01a669a502f5bbf509e7583dd
Bollwerker <Bollwerker@users.noreply.github.com>
2014-11-30 17:41:49 +0100

    Update DBLoopMinor.java

--- DBLoopMinor.java
+++ DBLoopMinor.java
@@ -15,6 +15,7 @@ import datenbank.Datenbank;
  */
 public class DBLoopMinor extends Thread
 {
+    //Vorbereitungen für Socket Übertragung
     private static ServerSocket serverSocket;
     private static Socket clientSocket;
     private static ObjectOutputStream oOut;
@@ -22,29 +23,34 @@ public class DBLoopMinor extends Thread
     private static LongBool longBool = new LongBool(0,false);
     private static ArrayList returnThis = new ArrayList();
     
-    public void run()
+    public void run() //spezielle "run" Methode um mit Thread zu arbeiten
     {
         while(true)
         {
             try
             {
+                //Socket öffnen und auf Anfrage mit ÜbergabeObjekt warten
                 serverSocket = new ServerSocket(55305);
                 clientSocket = serverSocket.accept();
                 oIn =  new ObjectInputStream(clientSocket.getInputStream());
                 try{longBool = (LongBool) oIn.readObject();}catch(ClassNotFoundException e){System.out.println(e);}
                 oIn.close();
+                //Ende des Empfangens
                 
                 //TODO Minor besorgen von DB
+                //Anfrage an die Datenbank vom Server mit den Übergabevariablen
                 returnThis = Datenbank.ausgeben(longBool.time,longBool.truefalse);
                 
+                //Rückgabe der ermittelten Programminformation'en in form einer ArrayList
                 oOut = new ObjectOutputStream(clientSocket.getOutputStream());
                 oOut.writeObject(returnThis);
                 oOut.flush();
                 oOut.close();
-                
+                //Ende des Sendens von Daten
                 serverSocket.close();
                 clientSocket.close();
-                try{Thread.sleep(50);}catch(Exception e){System.out.println(e);}
+                //Ender der Übertragung
+                try{Thread.sleep(50);}catch(Exception e){System.out.println(e);} //Sleep um Überlastung der Sockets zu unterbinden
             }
             catch(IOException e)
             {
