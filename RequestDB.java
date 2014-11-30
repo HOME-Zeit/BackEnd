3447319f1079833130a475fc6151eb1d5d9bf6a1
Bollwerker <Bollwerker@users.noreply.github.com>
2014-11-30 16:42:56 +0100

    Update RequestDB.java

--- RequestDB.java
+++ RequestDB.java
@@ -10,17 +10,17 @@ public class RequestDB
 {
     //Class<?> Programm = new server_FrontDBCon.Programminformation().getClass(); 
     
-    public static ArrayList requestMinor(long time, boolean truefalse)
+    public static ArrayList requestMinor(long time, boolean truefalse) //requestMinor fragt die Datenbank nach allen Programminformationen von -4h und +20h
     {
         //TODO get certain Stuff from DB
         return Datenbank.ausgeben(time,truefalse);
     }
-    public static ArrayList requestMajor(boolean truefalse)
+    public static ArrayList requestMajor(boolean truefalse) //requestMajor fragt die Datenbank nach ALLEN Programminformationen
     {
         //TODO get ALL STUFF from DB
         return Datenbank.ausgeben(truefalse);
     }
-    public static ArrayList requestNumber(int number, boolean truefalse)
+    public static ArrayList requestNumber(int number, boolean truefalse) //requestNumber fragt die Datenbank nach der Programminformation mit der mitgegebenen Seriennummer
     {
         //TODO get Stuff via number
         return Datenbank.ausgeben(number,truefalse);
