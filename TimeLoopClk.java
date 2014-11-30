f6179614aac6f0cde3db0753ddff2f4d64046c65
Bollwerker <Bollwerker@users.noreply.github.com>
2014-11-30 17:59:20 +0100

    Update TimeLoopClk.java

--- TimeLoopClk.java
+++ TimeLoopClk.java
@@ -15,6 +15,7 @@ import java.text.DateFormat;
  */
 public class TimeLoopClk extends Thread 
 {
+    //Vorbereitung der notwendigen variablen und objekte
     private static ServerSocket serverSocket;
     private static Socket clientSocket;
     private static PrintWriter printWriter;
@@ -29,24 +30,29 @@ public class TimeLoopClk extends Thread
         {
             try
             {
+                //aufbau der Verbindung und warten auf client
                 serverSocket = new ServerSocket(55303);
                 clientSocket = serverSocket.accept();
                 printWriter = new PrintWriter(clientSocket.getOutputStream(),true);
+                //nach eingang der anfrage die aktuelle zeit abrufen vom NTPServer
                 try
                 {
                     epoch = Retrieve_Time.getTime();
                     date = new Date(epoch);
                 }
+                //bei nichtverfügbarkeit die eigene Systemzeit benutzen
                 catch(Exception e)
                 {
                     epoch = Retrieve_Time.getTime_failed();
                     date = new Date(epoch);
                 }
+                //die ermittelte zeit senden über printWriter
                 printWriter.println(dateFormat.format(date));
                 printWriter.flush();
                 printWriter.close();
                 serverSocket.close();
                 clientSocket.close();
+                //ende der übertragung
                 try{Thread.sleep(50);}catch(Exception e){}
             }
             catch(IOException e)
